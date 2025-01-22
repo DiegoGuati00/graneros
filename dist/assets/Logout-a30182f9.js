57bc08878393d571ee8aa2390d3c0a7a1b9c7e21
diego guatibonza <diegoguati00@gmail.com>
2025-01-21 21:29:44 -0500

    cambios

--- dist/assets/Logout-a30182f9.js
+++
@@ -1,1 +0,0 @@
-import{r as o,a as e}from"./index-39a70ab6.js";const r=()=>(o.useEffect(()=>{sessionStorage.removeItem("session"),window.location.replace("/dashboard")},[]),e("div",{}));export{r as default};
